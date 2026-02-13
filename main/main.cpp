9837e43d5f2973b9ef2b266f9264ca0558b60259
Wei Cheng <wcheng95@gmail.com>
2026-02-12 21:15:27 -0800

    V1.3.3 fix TX stop/reset bug

--- main/main.cpp
+++ main/main.cpp
@@ -668,7 +668,7 @@ static std::vector<std::string> g_ctrl_lines = {
 };
 
 static std::vector<std::string> g_startup_lines = {
-    "Mini-FT8 V1.3.2",
+    "Mini-FT8 V1.3.3",
     "S: Status(Operate)",
     "R: Rx page",
     "T: Tx page",
